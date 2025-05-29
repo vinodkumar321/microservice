4d50b2812fa9330a27d6345477a01187e6af50e5
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2025-05-29 13:17:04 +0530

    Initial Commit

---
+++ src/test/java/com/javatpoint/microservice2/SpringMicroservces2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.microservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservces2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
